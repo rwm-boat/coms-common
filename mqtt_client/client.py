7e1e5f27886976e59c372c940c981a9426a95e06
Andrew Robbertz <a.robbertz@gmail.com>
2019-08-21 19:57:36 -0400

    added all callbacks

--- mqtt_client/client.py
+++ mqtt_client/client.py
@@ -1,49 +1,108 @@
-import paho.mqtt.client as mqtt #import the client1
+import paho.mqtt.client as mqtt 
+
 import time
 import argparse
+import logging
 
 
 class MQTTClient(mqtt.Client):
 
-    def __init__(self, client_id="", broker_address="pi-zero", on_connect = None, on_message_received= None):
+    def __init__(self, mqtt_client_id="", transport="tcp", broker_address="pi-zero", on_connect_ret=None, on_disconnect_ret=None, on_message_ret=None, on_publish_ret=None, on_subscribe_ret=None, on_unsubscribe_ret=None):
         
-        super().__init__(client_id)
+        super().__init__(client_id=mqtt_client_id)
 
         # Set the Initial Callbacks
-        if on_connect: 
-            self.on_connect = on_connect
+        if on_connect_ret is not None: 
+            self.on_connect = on_connect_ret
         else: 
-            self.on_connect = self._on_connect
-        if on_message_received:     
-            self.on_message = on_message_received
+            self.on_connect = self._on_connect_ret
+            
+        if on_disconnect_ret is not None: 
+            self.on_disconnect = on_disconnect_ret
         else: 
-            self.on_message= self._on_message_received
+            self.on_disconnect = self._on_disconnect_ret
+
+        if on_message_ret is not None:     
+            self.on_message = on_message_ret
+        else: 
+            self.on_message = self._on_message_ret
+
+        if on_publish_ret is not None:     
+            self.on_publish = on_publish_ret
+        else: 
+            self.on_publish = self._on_publish_ret
+    
+        if on_subscribe_ret is not None:     
+            self.on_subscribe = on_subscribe_ret
+        else: 
+            self.on_subscribe = self._on_subscribe_ret
+
+        if on_unsubscribe_ret is not None:     
+            self.on_unsubscribe = on_unsubscribe_ret
+        else: 
+            self.on_unsubscribe = self._on_unsubscribe_ret
 
         # Connect to the Broker
-        print(f"Attempting to conect to broker at {broker_address}")
-        self.connect(broker_address)
-        print("Successfully Connected to Broker")
-        # Maintain Connection
-        #self.loop_forever()
+        self.connect_to_broker(broker_address)
+
+
+    # Connects to the Boat Broker at given IP / hostname
+    def connect_to_broker(self, broker_address):
+        try:
+            print(f"Attempting to conect to broker at {broker_address}")
+            self.connect(broker_address)
+        except:
+            print(f"Faled to connect to broker at : {broker_address}")
+        else:
+            print(f"Successfuly connected to broker at : {broker_address}")
 
 
     # The callback for when the client receives a CONNACK response from the server.
-    def _on_connect(self, client, userdata, flags, rc, default_subs=[]):
-        print("Connected to Broker with result code "+str(rc))
+    def _on_connect_ret(self, client, userdata, flags, rc):
+        print(f"Connected to Broker with result code : {str(rc)}")
+
+    # Called when the client disconnects from the broker.
+    def _on_disconnect_ret(self, client, userdata, rc):
+        print(f"Disconnected from Server with result code : {str(rc)}")
+
+    ## 
+    # Called when a message has been received on a topic that the client subscribes to 
+    #  and the message does not match an existing topic filter callback. 
+    #  Use message_callback_add() [ add_specific_callback() ] to define a callback that will 
+    #  be called for specific topic filters. on_message will serve as fallback when none matched.
+    def _on_message_ret(self, client, userdata, message):
+        payload = str(message.payload.decode("utf-8"))
+        print(f"message received  {payload}")
+        print(f"message topic= {message.topic}")
+        print(f"message qos= {message.qos}")
+        print(f"message retain flag= {message.retain}")
+
+    ###
+    # This function allows you to define callbacks that handle incoming messages
+    #  for specific subscription filters, including with wildcards. This lets you, 
+    #  for example, subscribe to sensors/# and have one callback to handle sensors/temperature 
+    #  and another to handle sensors/humidity
+    def add_specific_callback(self, topic_filter, callback):
+        message_callback_add(topic_filter, callback)
+
+    
+    # Remove a topic/subscription specific callback previously registered
+    #  using message_callback_add() [ add_specific_callback() ]
+    def remove_secific_callback(self, topic_filter):
+        message_callback_remove(topic_filter)
+
+    
+    def _on_publish_ret(self, userdata, mid):
+        print(f"Message Published with mid : {str(mid)}")
 
 
-        # Subscribing in on_connect() means that if we lose the connection and
-        # reconnect then subscriptions will be renewed.
-        for topic in default_subs:
-            client.subscribe(topic)
+    def _on_subscribe_ret(self, client, userdata, mid, granted_qos):
+        print(f"Subscribed to topic with mid : {str(mid)} granted quality of service : {granted_qos}")
 
 
-    # The callback for when a PUBLISH message is received from the server.
-    def _on_message_received(self, client, userdata, message):
-        print("message received " ,str(message.payload.decode("utf-8")))
-        print("message topic=",message.topic)
-        print("message qos=",message.qos)
-        print("message retain flag=",message.retain)
+    def _on_unsubscribe_ret(self, client, userdata, mid):
+        print(f"Subscribed to topic with mid : {str(mid)}")
+
 
     def subscribe_to_topic(self, topic):
         self.subscribe(topic)
@@ -77,15 +136,13 @@ if __name__ == '__main__':
         client_id = "dummy_client"
         print(f"No Client ID provided, set to {client_id}")
   
-    print("Creating new MQTT Client:    {}")
-    client = MQTTClient(client_id, broker_address) #create new instance
+    #create new instance
+    client = MQTTClient(mqtt_client_id=client_id, transport="tcp", broker_address=broker_address) 
 
     client.loop_start()
 
-    print("Subscribing to topic")
     client.subscribe_to_topic("status/temp_senesor/celcius")
 
-    print("Publishing message")
     client.publish_message("status/temp_senesor/celcius","temp: 25")
 
     # Wait for the broker to retur the message
