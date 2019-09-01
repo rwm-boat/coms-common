9f6c008893fbc7ca1b9dd4d990fea50cdeba7ab8
Andrew Robbertz <a.robbertz@gmail.com>
2019-09-01 14:52:33 -0400

    testing Publisher implementation in Examples

--- mqtt_client/publisher.py
+++ mqtt_client/publisher.py
@@ -5,9 +5,9 @@ class Publisher():
     def __init__(self, client_id="default_publisher"):
         
         # Define Broker IP Address
-        broker_addresss = "192.168.1.70"
+        broker_ip = "192.168.1.70"
 
-        self.client = MQTTClient(mqtt_client_id=client_id, broker_address=broker_address)
+        self.client = MQTTClient(mqtt_client_id=client_id, broker_address=broker_ip)
 
     def publish(topic, message):
-        client.publish(topic, message)
+        client.publish(topic=topic, payload=message, reatain=True)
