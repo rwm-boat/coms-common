b06a5860c351401750cc9f3cb086abddef262229
alrobbertz <a.robbertz@gmail.com>
2019-09-29 23:39:56 -0400

    plotter looking nice

--- examples/plotter.py
+++ examples/plotter.py
@@ -1,10 +1,44 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import json
+import math
 
 
 
-i = 0
+
+time = []
+latitude = []
+longitude = []
+
 with open('log.txt', 'r') as log_file:
+    error_filter = 20
     for line in log_file.readlines():
-        print(line)
-        print(i)
+        obj = json.loads(line)
+        time.append(obj['time'])
+        if abs(obj['latitude']) > error_filter:
+            latitude.append(obj['latitude'])
+        if abs(obj['longitude']) > error_filter:
+            longitude.append(obj['longitude'])
+
+
+plt.subplot(1, 2, 1)
+plt.plot(latitude, label='Latitude')
+plt.title('Latitude')
+plt.xlabel('Time')
+plt.ylabel('Coords')
+plt.legend()
+plt.grid()
+
+plt.subplot(1, 2, 2)
+plt.plot(longitude, label='Longitude')
+plt.title('Longiude')
+plt.xlabel('Time')
+plt.ylabel('Coords')
+plt.legend()
+plt.grid()
+plt.show()
+
+with open('gps.csv', 'w') as gps:
+    gps.write("latitude,longitude\n")
+    for i in range(0, len(latitude)):
+        if i < len(latitude) and i < len(longitude):
+            gps.write(f"{latitude[i]},{longitude[i]}\n")
