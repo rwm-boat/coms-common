2843b4ddba281cd7a8d64cea4a8a89fba34c8c50
alrobbertz <a.robbertz@gmail.com>
2019-09-30 23:25:25 -0400

    logging on all status messages

--- examples/logger.py
+++ examples/logger.py
@@ -2,14 +2,27 @@ from mqtt_client.subscriber import Subscriber
 import json
 import time
 
-
 def on_gps_received(client, userdata, message):
-    #print("RECEIVED GPS DATA")
     obj = json.loads(message.payload.decode('utf-8'))
-    #print("time: " + str(obj['time']))
-    #print("latitude" + str(obj['latitude']))
-    #print("longitude" + str(obj['longitude']))
-    with open('log.txt', 'a') as outfile:
+    with open('gps_log.txt', 'a') as outfile:
+        json.dump(obj, outfile)
+        outfile.write('\n')
+
+def on_compass_received(client, userdata, message):
+    obj = json.loads(message.payload.decode('utf-8'))
+    with open('compas_log.txt', 'a') as outfile:
+        json.dump(obj, outfile)
+        outfile.write('\n')
+
+def on_temp_received(client, userdata, message):
+    obj = json.loads(message.payload.decode('utf-8'))
+    with open('temp_log.txt', 'a') as outfile:
+        json.dump(obj, outfile)
+        outfile.write('\n')
+
+def on_adc_received(client, userdata, message):
+    obj = json.loads(message.payload.decode('utf-8'))
+    with open('adc_log.txt', 'a') as outfile:
         json.dump(obj, outfile)
         outfile.write('\n')
 
@@ -20,15 +33,14 @@ def on_gps_received(client, userdata, message):
 if __name__ == '__main__':
 
     default_subscriptions = {
-        "/status/gps": on_gps_received
+        # Nav Loggers
+        "/status/gps": on_gps_received,
+        "/status/compass": on_compass_received,
+        # Jet Loggers
+        "/status/temp": on_temp_received,
+        "/status/adc": on_adc_received
     }
-     
     subber = Subscriber(client_id="logger_client", broker_ip="127.0.0.1", default_subscriptions=default_subscriptions)
-    # 
-    # subber.subscribe("/status/time", on_time_received)
-    # subber.subscribe("/status/gps", on_gps_received)
-    #subber.subscribe_many(default_subscriptions)
-
     subber.listen()
 
     while(True):
