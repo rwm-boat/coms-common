73b4860d199d057611de27b8626c1a32ed72861b
brentrolfes <brentrolfes20@gmail.com>
2019-10-07 16:24:30 -0400

    adding sync-logger

---
+++ mqtt_client/sync-logger.py
@@ -0,0 +1,112 @@
+import sys,os
+import curses
+import time
+from mqtt_client.subscriber import Subscriber
+from threading import Thread
+import json
+
+# global variables for logging
+mag_compass_reading = 0
+int_compass_reading = 0
+time_reading = 0
+lat_reading = 0
+lon_reading = 0
+speed_reading = 0
+gps_heading_reading = 0
+jet1_temp = 0
+jet2_temp = 0
+compartment_temp = 0
+gps_distance = 0
+jet1_current = 0 #starboard
+jet2_current = 0 #port
+
+def on_temp_received(client, userdata, message):
+    global jet1_temp
+    global jet2_temp
+    global compartment_temp
+
+    obj = json.loads(message.payload.decode('utf-8'))
+    jet1_temp = obj["jet1_temp"]
+    jet2_temp = obj["jet2_temp"]
+    compartment_temp = obj["compartment_temp"]
+
+def on_compass_received(client, userdata, message):
+    global mag_compass_reading
+    obj = json.loads(message.payload.decode('utf-8'))
+    mag_compass_reading = obj['compass']
+
+def on_internal_compass_received(client, userdata, message):
+    global int_compass_reading
+    obj = json.loads(message.payload.decode('utf-8'))
+    int_compass_reading = obj['heading']
+
+ddef on_gps_received(client, userdata, message):
+    # create global variables for UI
+    global time_reading
+    global lat_reading
+    global lon_reading
+    global speed_reading
+    global gps_heading_reading
+    global gps_distance
+    
+    obj = json.loads(message.payload.decode('utf-8'))
+
+    # parse json into global variablesspeed_reading,
+    time_reading = obj["time"]
+    lat_reading = obj['latitude']
+    lon_reading = obj['longitude']
+    speed_reading = obj["speed"]
+    gps_heading_reading = obj["course"]
+    gps_distance = obj['distance']
+
+def on_adc_received(client, userdata, message):
+    global jet1_current
+    global jet2_current
+
+    obj = json.loads(message.payload.decode('utf-8'))
+    jet1_current = obj["jet1_amps"]
+    jet2_current = obj["jet2_amps"]
+
+# ==================
+# -- MAIN METHOD -- 
+# ==================
+if __name__ == '__main__':
+
+    try:
+        default_subscriptions = {
+            "/status/compass": on_compass_received,
+            "/status/gps" : on_gps_received,
+            "/status/adc" : on_adc_received,
+            "/status/internal_compass" : on_internal_compass_received
+            "/status/temp" : on_temp_received
+        }
+        subber = Subscriber(client_id="telemetry_live", broker_ip="192.168.1.170", default_subscriptions=default_subscriptions)
+        thread = Thread(target=subber.listen)
+        thread.start()
+        print("here")
+
+        while True:
+            message = {
+                'time' : time_reading,
+                'speed' : speed_reading,
+                'jet1_current' : jet1_current,
+                'jet2_current' : jet2_current,
+                'mag_compass' : mag_compass_reading,
+                'int_compass' : int_compass_reading,
+                'gps_heading' :gps_heading_reading,
+                'latitude' : lat_reading,
+                'longitude' : lon_reading,
+                'distance' : gps_distance,
+                'jet1_temp' : jet1_temp,
+                'jet2_temp' : jet2_temp,
+                'compartment_temp' : compartment_temp,
+            }
+            sleep(0.1)
+            print(message)
+
+       
+        
+    except KeyboardInterrupt:
+        
+        print("-End Logger-")
+       
