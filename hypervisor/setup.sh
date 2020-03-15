361c6a9d4051cd6ebb25fee27818801b12733495
danielshrives <danielshrives@gmail.com>
2020-03-14 22:03:48 -0400

    Adding Info for Jenkins Pi

--- hypervisor/setup.sh
+++ hypervisor/setup.sh
@@ -1,6 +1,7 @@
 # !/bin/bash 
 
 # Add Both Raspberry Pi's to /etc/hosts
+echo "192.168.1.169 jenkins-pi" | sudo tee -a /etc/hosts
 echo "192.168.1.170 broker-pi" | sudo tee -a /etc/hosts
 echo "192.168.1.171 jet-pi" | sudo tee -a /etc/hosts
 echo "192.168.1.172 nav-pi" | sudo tee -a /etc/hosts
@@ -10,10 +11,12 @@ ssh-keygen -t rsa
 
 # Create a '/home/pi/.ssh' folder in each pi
 #   (Should have the folder already)
+ssh pi@broker-pi mkdir -p .ssh
 ssh pi@jet-pi mkdir -p .ssh
 ssh pi@nav-pi mkdir -p .ssh
 
 # Copy The Created SSH keys to Jet and Nav
+cat ~/.ssh/id_rsa.pub | ssh pi@broker-pi 'cat >> .ssh/authorized_keys'
 cat ~/.ssh/id_rsa.pub | ssh pi@jet-pi 'cat >> .ssh/authorized_keys'
 cat ~/.ssh/id_rsa.pub | ssh pi@nav-pi 'cat >> .ssh/authorized_keys'
 
