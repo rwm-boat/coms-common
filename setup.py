20bc2a1052de96bc4bb211aca515205b6646d459
danielshrives <danielshrives@gmail.com>
2020-03-20 22:49:40 +0000

    added andrews emial to setup

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import find_packages, setup
 
+
 setup(
     name="mqtt_client",
     version="1.0.0",
     author="Andrew Robbertz",
+    author_email="a.robbertz@gmail.com",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
